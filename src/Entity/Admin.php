9b7b316c0f15ede95ad78fd752ca60eb00fa351a
moussaillon <100614972+Ramatoulaye23@users.noreply.github.com>
2024-07-10 14:52:53 +0200

    creation user

---
+++ src/Entity/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdminRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdminRepository::class)]
+class Admin
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
