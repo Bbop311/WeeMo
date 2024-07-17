f55815ab97fc1eb64eb1368d570a7cf16fb90b4b
Bbop311 <bbops311@gmail.com>
2024-07-17 16:47:27 +0200

    Refactor Data Fixtures
    -made seperate files for each Fixtures
    -Created the dependencies so that the fixtures are made in the right order
    -Created the references between the fixtures

---
+++ src/DataFixtures/PropertyFeaturesFixtures.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\PropertyFeatures;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\UserFixtures;
+use App\DataFixtures\PropertyFixtures;
+
+class PropertyFeaturesFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $propertyfeatures = [
+            [
+                "id" => 1,
+                "property_id" => 1010,
+                "type_of_rooms" => "T3",
+                "number_of_bedrooms" => 3,
+                "floor" => 2,
+                "property_condition" => "good",
+                "energy_class" => "B",
+                "elevator" => true,
+                "balcony" => true,
+                "parking" => true,
+                "heating_type" => "gas",
+                "air_condition" => false
+            ],
+            [
+                "id" => 2,
+                "property_id" => 1011,
+                "type_of_rooms" => "T1",
+                "number_of_bedrooms" => 1,
+                "floor" => 3,
+                "property_condition" => "needs renovation",
+                "energy_class" => "C",
+                "elevator" => false,
+                "balcony" => false,
+                "parking" => true,
+                "heating_type" => "electric",
+                "air_condition" => true
+            ],
+            [
+                "id" => 3,
+                "property_id" => 1012,
+                "type_of_rooms" => "studio",
+                "number_of_bedrooms" => 1,
+                "floor" => 1,
+                "property_condition" => "new",
+                "energy_class" => "A",
+                "elevator" => true,
+                "balcony" => true,
+                "parking" => false,
+                "heating_type" => "central",
+                "air_condition" => true
+            ],
+            [
+                "id" => 4,
+                "property_id" => 1013,
+                "type_of_rooms" => "T4",
+                "number_of_bedrooms" => 4,
+                "floor" => 4,
+                "property_condition" => "good",
+                "energy_class" => "D",
+                "elevator" => false,
+                "balcony" => true,
+                "parking" => true,
+                "heating_type" => "wood",
+                "air_condition" => false
+            ],
+            [
+                "id" => 5,
+                "property_id" => 1014,
+                "type_of_rooms" => "T2",
+                "number_of_bedrooms" => 2,
+                "floor" => 5,
+                "property_condition" => "needs renovation",
+                "energy_class" => "E",
+                "elevator" => true,
+                "balcony" => true,
+                "parking" => true,
+                "heating_type" => "electric",
+                "air_condition" => true
+            ],
+            [
+                "id" => 6,
+                "property_id" => 1015,
+                "type_of_rooms" => "T5",
+                "number_of_bedrooms" => 5,
+                "floor" => 7, 
+                "property_condition" => "new",
+                "energy_class" => "F",
+                "elevator" => false,
+                "balcony" => true,
+                "parking" => false,
+                "heating_type" => "gas",
+                "air_condition" => false
+            ],
+            [
+                "id" => 7,
+                "property_id" => 1016,
+                "type_of_rooms" => "T6",
+                "number_of_bedrooms" => 6,
+                "floor" => 6,
+                "property_condition" => "good",
+                "energy_class" => "G",
+                "elevator" => true,
+                "balcony" => false,
+                "parking" => true,
+                "heating_type" => "central",
+                "air_condition" => true
+            ],
+            [
+                "id" => 8,
+                "property_id" => 1017,
+                "type_of_rooms" => "T1",
+                "number_of_bedrooms" => 1,
+                "floor" => 1, 
+                "property_condition" => "needs renovation",
+                "energy_class" => "A",
+                "elevator" => false,
+                "balcony" => true,
+                "parking" => false,
+                "heating_type" => "wood",
+                "air_condition" => true
+            ],
+            [
+                "id" => 9,
+                "property_id" => 1018,
+                "type_of_rooms" => "T2",
+                "number_of_bedrooms" => 2,
+                "floor" => 1,
+                "property_condition" => "new",
+                "energy_class" => "B",
+                "elevator" => true,
+                "balcony" => false,
+                "parking" => true,
+                "heating_type" => "electric",
+                "air_condition" => false
+            ],
+            [
+                "id" => 10,
+                "property_id" => 1019,
+                "type_of_rooms" => "studio",
+                "number_of_bedrooms" => 1,
+                "floor" => 2,
+                "property_condition" => "good",
+                "energy_class" => "C",
+                "elevator" => true,
+                "balcony" => true,
+                "parking" => true,
+                "heating_type" => "gas",
+                "air_condition" => true
+            ],
+            [
+                "id" => 11,
+                "property_id" => 1020,
+                "type_of_rooms" => "T3",
+                "number_of_bedrooms" => 3,
+                "floor" => 3,
+                "property_condition" => "needs renovation",
+                "energy_class" => "D",
+                "elevator" => false,
+                "balcony" => false,
+                "parking" => true,
+                "heating_type" => "central",
+                "air_condition" => false
+            ],
+            [
+                "id" => 12,
+                "property_id" => 1021,
+                "type_of_rooms" => "T4",
+                "number_of_bedrooms" => 4,
+                "floor" => 4,
+                "property_condition" => "new",
+                "energy_class" => "E",
+                "elevator" => true,
+                "balcony" => true,
+                "parking" => false,
+                "heating_type" => "wood",
+                "air_condition" => true
+            ],
+            [
+                "id" => 13,
+                "property_id" => 1022,
+                "type_of_rooms" => "T5",
+                "number_of_bedrooms" => 5,
+                "floor" => 5,
+                "property_condition" => "good",
+                "energy_class" => "F",
+                "elevator" => false,
+                "balcony" => true,
+                "parking" => true,
+                "heating_type" => "electric",
+                "air_condition" => true
+            ],
+            [
+                "id" => 14,
+                "property_id" => 1023,
+                "type_of_rooms" => "T6",
+                "number_of_bedrooms" => 6,
+                "floor" => 7, 
+                "property_condition" => "needs renovation",
+                "energy_class" => "G",
+                "elevator" => true,
+                "balcony" => false,
+                "parking" => true,
+                "heating_type" => "gas",
+                "air_condition" => false
+            ]
+        ];
+    
+        $i=1;
+        foreach ($propertyfeatures as $propertyfeatureData) {
+            $propertyfeature = new PropertyFeatures;
+
+           $propertyfeature->setProperty($this->getReference('property_' . $i));
+           $propertyfeature->setTypeOfRooms($propertyfeatureData['type_of_rooms']);
+           $propertyfeature->setNumberOfBedrooms($propertyfeatureData['number_of_bedrooms']);
+
+           $propertyfeature->setFloor($propertyfeatureData['floor']);
+           $propertyfeature->setPropertyCondition($propertyfeatureData['property_condition']);
+          
+           $propertyfeature->setEnergyClass($propertyfeatureData['energy_class']);
+           $propertyfeature->setElevator($propertyfeatureData['elevator']);
+           $propertyfeature->setBalcony($propertyfeatureData['balcony']);
+           $propertyfeature->setParking($propertyfeatureData['parking']);
+           $propertyfeature->setHeatingType($propertyfeatureData['heating_type']);
+           $propertyfeature->setAirCondition($propertyfeatureData['air_condition']);
+         
+           $manager->persist($propertyfeature);
+           $i++;
+       }
+       $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+            PropertyFixtures::class,
+        ];
+    }
+}
---
+++ src/DataFixtures/PropertyFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Property;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use PDO;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\UserFixtures;
+
+class PropertyFixtures extends Fixture implements DependentFixtureInterface
+{
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher, )
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Code taken from Julien, it uses a database "recupdb" to load the fixtures with everything in the database, it requires the recupdb database to exist though (with the dump imported)
+        $dbUser = "root";
+        $dbPwd = "root";
+
+        $pdoDb = new PDO('mysql:host=127.0.0.1; dbname=recupdb', $dbUser, $dbPwd);
+        $smt = $pdoDb->prepare('SELECT * FROM property');
+        $smt->execute();
+        $immos = $smt->fetchAll();
+
+        foreach ($immos as $immo) {
+            $property = new Property();
+            $user_count = 
+            $property
+            ->setNatureMutation($immo['nature_mutation'])
+            ->setDateMutation($immo['date_mutation'])
+            ->setValeurFonciere($immo['valeur_fonciere'])
+            ->setNoVoie($immo['no_voie'])
+            ->setBTQ($immo['b_t_q'])
+            ->setTypeVoie($immo['type_voie'])
+            ->setCodeVoie($immo['code_voie'])
+            ->setVoie($immo['voie'])
+            ->setCodePostal($immo['code_postal'])
+            ->setCommune($immo['commune'])
+            ->setCodeDepartement($immo['code_departement'])
+            ->setCodeCommune($immo['code_commune'])
+            ->setSection($immo['section'])
+            ->setNbLots($immo['nb_lots'])
+            ->setCodeTypeLocal($immo['code_type_local'])
+            ->setTypeLocal($immo['type_local'])
+            ->setSurfaceReelleBati($immo['surface_reelle_bati'])
+            ->setNbPieces($immo['nb_pieces'])
+            ->setSurfaceTerrain($immo['surface_terrain'])
+            ->setUser($this->getReference('user_' . rand(0, count(UserFixtures::USERS)-1)));
+
+            $manager->persist($property);
+
+            $this->addReference('property_' . $immo['id'], $property);
+        }
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+
+}
+
+    
+
