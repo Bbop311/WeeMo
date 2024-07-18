1424c2839eb4aa5bb9f9f1ff47358d6cf7194315
Bbop311 <bbops311@gmail.com>
2024-07-18 16:58:02 +0200

    created link to validation page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+use App\Form\ListingValidationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ListingRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function adminDashboard(): Response
+    public function adminDashboard(ListingRepository $listingRepository): Response
     {
+        $listings = $listingRepository->findAll();
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        
+
         return $this->render('admin/index.html.twig', [
+            'listings' => $listings,
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/listing_validation/{listing_id}', name: 'listing_validation')]
+    public function listing_validation(Request $request, ListingRepository $listingRepository, int $listing_id): Response
+    {
+        $listing = $listingRepository->find($listing_id);
+
+        $form = $this->createForm(ListingValidationType::class, $listing);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $listing->setStatus($form->get('status')->getData());
+            dd($listing);
+        }
+        return $this->render('admin/listing_validation.html.twig', [
+            'form' => $form,
+            'listing' => $listing
         ]);
     }
 }
