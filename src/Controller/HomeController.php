499deff3c149b92bc253dcc4f47af20229a10b01
Bbop311 <bbops311@gmail.com>
2024-07-19 16:31:46 +0200

    Readd search form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,43 +9,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\PropertyRepository;
 use App\Service\propertyListGenerator;
+use Knp\Component\Pager\PaginatorInterface;
 
 
 class HomeController extends AbstractController
 {
-    #[Route('/home/{page}', name: 'app_home')]
-    public function index(Request $request, PropertyRepository $propertyRepository, propertyListGenerator $propertyListGenerator, int $page = 1): Response
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request, PropertyRepository $propertyRepository, PaginatorInterface $paginator, int $page = 1): Response
     {
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
 
-        $properties_list = $propertyListGenerator->getList($propertyRepository);
-        $properties = [];
-        for ($i = 24*($page-1) ; $i < 24*($page) ;$i++ )
-        {
-            $properties[] = $properties_list[$i];
-        }
-        dump($form);
-        if($request->getMethod() === 'POST') dd($form->isValid()) ;
-        if ($form->isSubmitted() && $form->isValid()) {
-            // dd($request->request);
-            // $foo = $_GET['Ville'];
-            // $var = $request->request;
-            $data = $form->getData();
-            dd($data);
-        }
+        $queryBuilder = $propertyRepository->createQueryBuilder('p');
+        $queryBuilder->orderBy('p.id', 'ASC');
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page',1),
+            24
+        );
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'properties' => $properties,
-            'page' => $page,
+            'pagination' => $pagination,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/', name: 'home')]
-    public function redirect_to_home(): Response
-    {
-        return $this->redirectToRoute('app_home');
     }
 
     #[Route('/property/{id}', name:'property_show')]
